20a41fd1457c632203a359d7fb9178cc7078a37a
Patrick <patrickpeterudo@gmail.com>
2022-09-20 14:16:18 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,11 @@
 *print_rev -> printing a string in reverse
 *@s: the string to be printed in reverse
 *return: nothing
-*/ 
-
+*/
 void print_rev(char *s)
 {
 int len = strlen(s);
-
 while len (len--)
-
 putchar(*(s + len));
 putchar(10);
 }
