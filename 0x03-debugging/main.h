3f8fb092b7af9894142d7970681b128fd8a56e29
Patrick <patrickpeterudo@gmail.com>
2022-10-02 08:46:40 -0700

    My main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,13 +1,17 @@
-#include <unistd.h>
+#ifndef MAIN_H
 
-/**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
+
+int convert_day(int month, int day);
+
+void print_times_table(int n);
+
+#endif /* MAIN_H */
