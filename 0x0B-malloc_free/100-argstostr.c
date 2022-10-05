6f13924e08e6610efc12bc7a9b9348d4c39d3c9a
Patrick <patrickpeterudo@gmail.com>
2022-10-05 14:52:49 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,6 +8,7 @@
 * Return: int
 */
 int _strlen(char *s)
+{
 int size = 0;
 for (; s[size] != '\0'; size++)
 ;
@@ -32,7 +33,6 @@ s = malloc(sizeof(char) * nc + 1);
 if (s == 0)
 return (NULL);
 for (i = 0; i < ac; i++)
-{
 for (j = 0; av[i][j] != '\0'; j++, cmpt++)
 s[cmpt] = av[i][j];
 s[cmpt] = '\n';
